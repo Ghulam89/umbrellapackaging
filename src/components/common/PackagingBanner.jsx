3a3b3d5663a8af8f5bea0bd7b8f7ad21de21dd4e
Ghulam89 <gm6681328@gmail.com>
2025-03-24 11:49:13 -0700

    update

--- src/components/common/PackagingBanner.jsx
+++ src/components/common/PackagingBanner.jsx
@@ -7,7 +7,7 @@ function PackagingBanner({
 }) {
   return (
     
-    <div className='max-w-[1200px] my-2 mt-5 mx-auto rounded-[8px] h-[300px] p-5 bg-[#00000084] bg-cover bg-no-repeat bg-blend-multiply' style={{backgroundImage:`url(${bgImage})`}}>
+    <div className=' sm:max-w-7xl max-w-[95%] my-2 mt-5 mx-auto rounded-[8px] h-[300px] p-5 bg-[#00000084] bg-cover bg-no-repeat bg-blend-multiply' style={{backgroundImage:`url(${bgImage})`}}>
      <div className='flex flex-col justify-center items-center h-[300px]'>
      <h6 className='text-center text-[14px] text-[#E97900] font-semibold' >{title}</h6>
       <p className='md:text-[38px] text-[25px] font-semibold text-center text-[#fff]' >{subTitle}</p>
